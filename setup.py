4756d2c86d46955fd66439be018db5d6dd36df1a
vinh khuong <vinhkhuongquang@gmail.com>
2025-10-22 13:06:08 +0700

    requirement added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vinhkq",
+    author_email="vinhkhuongquang@gmail.com",
+    packages=find_packages()
+)
